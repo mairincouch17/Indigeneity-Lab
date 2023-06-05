d96dc311ff2f95d79c699d88319b85e91ad79ebd
mairincouch17 <135558351+mairincouch17@users.noreply.github.com>
2023-06-05 09:31:48 -0500

    Update google-doc-url.js
    Added personalized google API code

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/178k9ParMQFw21VBNieDgIpFLQlNyLPRnOJQ7ifYxvHo/edit#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyAkEbwOMSG1y2qgysgbUndX33QT_eDr2GY';
